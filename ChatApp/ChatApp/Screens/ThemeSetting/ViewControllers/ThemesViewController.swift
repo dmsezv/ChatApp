a3006c4db48eba3916b072259ece5d88c2c49b0f
Dmitrii <dmsezv2@gmail.com>
2021-03-10 00:08:03 +0300

    theme setting touch labels

--- ChatApp/ChatApp/Screens/ThemeSetting/ViewControllers/ThemesViewController.swift
+++ ChatApp/ChatApp/Screens/ThemeSetting/ViewControllers/ThemesViewController.swift
@@ -58,11 +58,12 @@ class ThemesViewController: UIViewController {
         if let chooseThemeButtonView = ChooseThemeButtonView.instanceFromNib() {
             chooseThemeButtonView.configureThemeButtonView(.night,  parentBounds: themeDayView.bounds)
             themeNightView.addSubview(chooseThemeButtonView)
+            themeNightView.tag = ThemeTypes.night.rawValue
             configureTheme(themeNightView)
 
             themeNightLabel.text = "Night"
             themeNightLabel.tag = ThemeTypes.night.rawValue
-            configureTheme(themeClassicLabel)
+            configureTheme(themeNightLabel)
         }
     }
     
@@ -70,11 +71,12 @@ class ThemesViewController: UIViewController {
         if let chooseThemeButtonView = ChooseThemeButtonView.instanceFromNib() {
             chooseThemeButtonView.configureThemeButtonView(.day, parentBounds: themeDayView.bounds)
             themeDayView.addSubview(chooseThemeButtonView)
+            themeDayView.tag = ThemeTypes.day.rawValue
             configureTheme(themeDayView)
 
             themeDayLabel.text = "Day"
             themeDayLabel.tag = ThemeTypes.day.rawValue
-            configureTheme(themeClassicLabel)
+            configureTheme(themeDayLabel)
         }
     }
     
@@ -82,6 +84,7 @@ class ThemesViewController: UIViewController {
         if let chooseThemeButtonView = ChooseThemeButtonView.instanceFromNib() {
             chooseThemeButtonView.configureThemeButtonView(.classic,  parentBounds: themeDayView.bounds)
             themeClassicView.addSubview(chooseThemeButtonView)
+            themeClassicView.tag = ThemeTypes.classic.rawValue
             configureTheme(themeClassicView)
             
             themeClassicLabel.text = "Classic"
@@ -112,6 +115,10 @@ class ThemesViewController: UIViewController {
         
         view.layer.borderWidth = 2
     }
+    
+    private func changeTheme(type: ThemeTypes) {
+        
+    }
 }
 
 
@@ -127,9 +134,18 @@ extension ThemesViewController {
     @objc private func touchThemeLabel(_ sender: UITapGestureRecognizer) {
         if let label = sender.view as? UILabel {
             switch label.tag {
-            case ThemeTypes.classic.rawValue: select(themeClassicView)
-            case ThemeTypes.day.rawValue: select(themeDayView)
-            case ThemeTypes.night.rawValue: select(themeNightView)
+            case ThemeTypes.classic.rawValue:
+                if let view = themeClassicView {
+                    select(view)
+                }
+            case ThemeTypes.day.rawValue:
+                if let view = themeDayView {
+                    select(view)
+                }
+            case ThemeTypes.night.rawValue:
+                if let view = themeNightView {
+                    select(view)
+                }
             default: break
             }
         }
