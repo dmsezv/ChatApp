8267faf28a080e318a5fcd4329e0b741d76aaa5a
Dmitrii <dmsezv2@gmail.com>
2021-04-20 01:42:30 +0300

    added collection view

--- ChatApp/ChatApp/PresentationLayer/Scenes/AvatarNetwork/AvatarNetworkViewController.swift
+++ ChatApp/ChatApp/PresentationLayer/Scenes/AvatarNetwork/AvatarNetworkViewController.swift
@@ -17,14 +17,73 @@ class AvatarNetworkViewController: UIViewController {
     
     // MARK: - IBOutlets
     
+    @IBOutlet weak var collectionView: UICollectionView!
     @IBAction func touchButtonClose(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
     
+    // MARK: - Drawing Constraints
+    
+    let spacing: CGFloat = 8
+    
+    
+    // MARK: - Life Cycle
+    
+    let avatarCellId = String(describing: AvatarCollectionViewCell.self)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupView()
+    }
+    
+    private func setupView() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: spacing, left: spacing, bottom: spacing, right: spacing)
+        layout.minimumLineSpacing = spacing
+        layout.minimumInteritemSpacing = spacing
+        collectionView.collectionViewLayout = layout
+    }
 }
 
 // MARK: - Display Logic
 
 extension AvatarNetworkViewController: AvatarNetworkViewControllerDisplayLogic {
     
+}
+
+// MARK: - UICollectionViewDelegate, UICollectionViewDataSource
+
+extension AvatarNetworkViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        100
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: avatarCellId, for: indexPath) as? AvatarCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        cell.configure()
+        
+        return cell
+    }
+}
+
+extension AvatarNetworkViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = (collectionView.bounds.width - 4 * spacing) / 3
+        return CGSize(width: width, height: width)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        spacing
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        spacing
+    }
 }
---
+++ ChatApp/ChatApp/PresentationLayer/Scenes/AvatarNetwork/Cells/AvatarCollectionViewCell.swift
@@ -0,0 +1,54 @@
+//
+//  AvatarCollectionViewCell.swift
+//  ChatApp
+//
+//  Created by Dmitrii Zverev on 20.04.2021.
+//
+
+import UIKit
+
+class AvatarCollectionViewCell: UICollectionViewCell {
+    
+    // MARK: Views
+    
+    private lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleToFill
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private lazy var activityIndicator: UIActivityIndicatorView = {
+        let activityIndicator = UIActivityIndicatorView()
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.color = .lightGray
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        return activityIndicator
+    }()
+    
+    // MARK: - Setup
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupLayout()
+    }
+    
+    private func setupLayout() {
+        addSubview(activityIndicator)
+        
+        NSLayoutConstraint.activate([
+            activityIndicator.centerXAnchor.constraint(equalTo: centerXAnchor),
+            activityIndicator.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+    }
+    
+    func configure() {
+        backgroundColor = .black
+        activityIndicator.startAnimating()
+    }
+}
